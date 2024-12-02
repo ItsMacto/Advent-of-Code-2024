898e9e258657477cf38b9164ac24419e8afe2bf7
Mac Howe <mac@machowe.com>
2024-12-02 10:46:44 -0500

    brute force-ish but still runs fast and got second star ⭐️

--- day2/day2.py
+++ day2/day2.py
@@ -28,70 +28,45 @@ def star1():
         
     return total
 
-    
+# 5 2 6 7 8    
+# 5 2 6 1 0
 def star2():
-    with open('test.txt') as f:
+    with open('input.txt') as f:
         data = f.read().splitlines()
 
     data = [list(map(int, x.split())) for x in data]
     total = 0
-    updated = []
+    
     for report in data:
-        print(report)
-        safe = True
-        if report[0] < report[1]:
-            for i in range(1, len(report)):
-                diff = report[i] - report[i-1]
-                if diff > 3 or diff < 1:
-                    safe = False
-                    report.pop(i - 1)
-                    updated.append(report)
-                    break
-
-                    
-        else:
-            for i in range(1, len(report)):
-                diff = report[i-1] - report[i]
-                if diff > 3 or diff < 1:
-                    safe = False
-                    report.pop(i - 1)
-                    updated.append(report)
-                    break
+        safe = False
+        for i in range(len(report)):
+            newReport = report[0:i] + report[i+1:]
+            if isSafe(newReport):
+                safe = True
+                break
         if safe:
-            print('safe', safe)
             total += 1
             
-            
-    for report in updated:
-        # print(report)
-        safe = True
-        if report[0] < report[1]:
-            for i in range(1, len(report)):
-                diff = report[i] - report[i-1]
-                if diff > 3 or diff < 1:
-                    safe = False
-                    # report.pop(i - 1)
-                    # updated.append(report)
-                    break
-
-                    
-        else:
-            for i in range(1, len(report)):
-                diff = report[i-1] - report[i]
-                if diff > 3 or diff < 1:
-                    safe = False
-                    # report.pop(i - 1)
-                    # updated.append(report)
-                    break
-        if safe:
-            print('safe', safe)
-            total += 1
-
     return total
+
+def isSafe(report):
+    safe = True
+    if report[0] < report[1]:
+        for i in range(1, len(report)):
+            diff = report[i] - report[i-1]
+            if diff > 3 or diff < 1:
+                safe = False
+                break
+    else:
+        for i in range(1, len(report)):
+            diff = report[i-1] - report[i]
+            if diff > 3 or diff < 1:
+                safe = False
+                break
+    return safe
     
 
 if __name__ == '__main__':
     print(star1())
-    print(star2())
-    
+    print(star2())    
     
