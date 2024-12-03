2bed0a0494c90438040a917c149093ec441e47a4
Mac Howe <mac@machowe.com>
2024-12-02 21:02:31 -0500

    small refactor

--- day2/day2.py
+++ day2/day2.py
@@ -1,4 +1,18 @@
-
+def isSafe(report):
+    safe = True
+    if report[0] < report[1]:
+        for i in range(1, len(report)):
+            diff = report[i] - report[i-1]
+            if diff > 3 or diff < 1:
+                safe = False
+                break
+    else:
+        for i in range(1, len(report)):
+            diff = report[i-1] - report[i]
+            if diff > 3 or diff < 1:
+                safe = False
+                break
+    return safe
 
 def star1():
     with open('input.txt') as f:
@@ -49,21 +63,6 @@ def star2():
             
     return total
 
-def isSafe(report):
-    safe = True
-    if report[0] < report[1]:
-        for i in range(1, len(report)):
-            diff = report[i] - report[i-1]
-            if diff > 3 or diff < 1:
-                safe = False
-                break
-    else:
-        for i in range(1, len(report)):
-            diff = report[i-1] - report[i]
-            if diff > 3 or diff < 1:
-                safe = False
-                break
-    return safe
     
 
 if __name__ == '__main__':
